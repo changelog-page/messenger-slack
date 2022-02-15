fb903f0b8db6f6bf7de035bf98a4e0d9023c5385
Brian Faust <hello@basecode.sh>
2022-02-15 04:15:21 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/Messenger.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Changelog\Messenger\Slack;
+
+use Changelog\Messenger\Messenger as Contract;
+use Illuminate\Support\Facades\Http;
+
+final class Messenger implements Contract
+{
+    public function message(string $from, string $to, string $text, ?array $options): void
+    {
+        Http::post($to, ['type' => 'mrkdwn', 'text' => $text]);
+    }
+}
